0e3c8906d06e7f7b97669f09362d757339383a8f
Mustafa Kurt <komacan@me.com>
2018-01-26 23:12:17 -0500

    updating Get method

--- myFirstWebApi/myFirstWebApi/Controllers/ValuesController.cs
+++ myFirstWebApi/myFirstWebApi/Controllers/ValuesController.cs
@@ -20,7 +20,7 @@ namespace myFirstWebApi.Controllers
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            return $"value {id}";
         }
 
         // POST api/values
